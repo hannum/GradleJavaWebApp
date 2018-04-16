357d479f3cb03442e028ccfd9c3a09b1a7cb7a77
Hannu Markkanen <hannu.markkanen@metropolia.fi>
2018-04-16 21:30:43 +0300

    Add integration tests

---
+++ src/integTest/java/dummyIntegrationTest.java
@@ -0,0 +1,26 @@
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author hannutam
+ */
+public class dummyIntegrationTest {
+
+    @Test
+    public void dummyIntegrationTest1() {
+        //System.out.println("Dummy test...");
+
+        assertEquals(0, 0);
+    }
+	
+	    @Test
+    public void dummyIntegrationTest2() {
+        //System.out.println("Dummy test...");
+
+        assertEquals(0, 1);
+    }
+
+
+}
--- src/main/java/org/gradle/demo/HelloServlet.java
+++ src/main/java/org/gradle/demo/HelloServlet.java
@@ -1,4 +1,4 @@
-/*
+/*-
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -21,14 +21,15 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
-        response.getWriter().print("Hello there, World!");  
+        response.getWriter().print("Hello there, World!"); 
+		response.getWriter().flush();		
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
+        Greeter greeter = new Greeter();
         String name = request.getParameter("name");
-        if (name == null) name = "World";
-        request.setAttribute("user", name);
-        request.getRequestDispatcher("response.jsp").forward(request, response); 
+        request.setAttribute("user", greeter.greet(name));
+        request.getRequestDispatcher("response.jsp").forward(request, response);
     }
 }
