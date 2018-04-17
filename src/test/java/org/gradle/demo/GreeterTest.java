78fa0dd8d66dfa19311285f052fb7cae5a123d8b
Hannu Markkanen <hannu.markkanen@metropolia.fi>
2018-04-17 11:04:12 +0300

    Add unit tests

--- src/test/java/org/gradle/demo/GreeterTest.java
+++ src/test/java/org/gradle/demo/GreeterTest.java
@@ -1,27 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package org.gradle.demo;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-/**
- *
- * @author hannutam
- */
 public class GreeterTest {
+
+    @Test
+    public void dummyUnitTest1() {
+        assertEquals(0, 1);
+    }
+    
+    @Test
+    public void dummyUnitTest2() {
+        assertEquals(0, 0);
+    }
+    
     @Test
     public void testGreet() {
 		//System.out.println("testGreet");
         Greeter greeter = new Greeter();
         assertEquals("Hello, Tester", greeter.greet("Tester"));
         assertEquals("Hello, World", greeter.greet(null));
-    }
-     @Test
-    public void dummyUnitTest() {
-        assertEquals(0, 1);
     }
 }
